5027b14fb8f143c9950f945b14d1fa1bb2554abe
Flakes <ed.afram@gmail.com>
2020-07-21 23:30:34 -0300

    script included for put online through ngrok application.

---
+++ SocialNetworking.Backend/APIRest/PutOnline.ps1
@@ -0,0 +1,8 @@
+﻿dotnet publish -r win-x64 --self-contained true -c Release
+
+cd bin\Release\netcoreapp3.1\win-x64
+
+Start-Process "APIRest.exe"
+Start-Process "cmd" -ArgumentList "/k ngrok http 5000"
+
+exit
